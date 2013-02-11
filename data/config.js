86885a920803fc303c76680d61af03b9ab5b9b3b
flabbyrabbit <flabbyrabbit@gmail.com>
2013-02-11 01:29:27 +0000

    Adding example data files

---
+++ data/config.js
@@ -0,0 +1,23 @@
+{
+	"server":"irc.hackthis.co.uk",
+	"channels":["#test"],
+	"nick":"TestBot",
+
+	"groove":"api_key",
+
+	"lastfm_key":"api_key",
+	"lastfm_secret":"api_secrect",
+
+	"bitly_user":"username",
+	"bitly_key":"api_key",
+
+	"mysql_db":"localhost",
+	"mysql_user":"username",
+	"mysql_pass":"password",
+	"mysql_db": "database",
+
+	"greeting": ["Konnichiwa", "Welcome"],
+
+	"modules": ["lastfm", "replace", "grooveshark", "issues", "greeting"],
+	"admin": ["admin"]
+}
