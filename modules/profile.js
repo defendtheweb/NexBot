89f1f810216285455980303664551bdc18029762
flabbyrabbit <flabbyrabbit@gmail.com>
2013-02-11 15:32:51 +0000

    Profiles module gets a minified user profile from HackThis (see #14)

---
+++ modules/profile.js
@@ -0,0 +1,37 @@
+var Profile = function() {
+	//Constructor
+}
+
+Profile.prototype = {
+	https: require('https'),
+	api_key: global.config.get('hackthis_api'),
+	handle: function(from, chan, message) {
+		var irc = global.irc;
+
+		if (matches = message.match(/^!([\S]*) (.*)$/i)) {
+			if (matches[1] == "profile") {
+				var req = this.https.request('https://api.hackthis.co.uk/?api_key='+this.api_key+'&method=user.getInfo&user='+matches[2], function(res) {
+					res.on('data', function(d) {
+						// Get the result and turn it into a JSON object.
+						var obj = JSON.parse(d);
+
+						if (obj.data) {
+							// Return the string.
+							var result = obj.data.user + " | Score: " + obj.data.score + " | Posts: " + obj.data.posts + " | Consecutive: " + obj.data.consecutive;
+							irc.client.say(chan, result);
+						} else {
+							irc.client.say(chan, "User not found");
+						}
+					});
+				});
+				req.end();
+
+				req.on('error', function(e) {
+					irc.client.say(chan, "Errm there's be an error");
+				});
+			}
+		}
+	}
+};
+
+module.exports = new Profile();
