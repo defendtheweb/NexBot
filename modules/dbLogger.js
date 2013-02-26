21f48f36822e5a8bdd23fbbb635aa9d903409894
flabbyrabbit <flabbyrabbit@gmail.com>
2013-02-26 15:58:22 +0000

    Added reconnection and replaced escaping to built in function of MySQL class

--- modules/dbLogger.js
+++ modules/dbLogger.js
@@ -2,17 +2,37 @@ var DB = function() {
 	//Constructor
 	var _mysql = require('mysql');
 	this.squel = require("squel");
-	this.mysql = _mysql.createConnection({
-	    user: global.config.get('mysql_user'),
-	    password: global.config.get('mysql_pass'),
-	    host: global.config.get('mysql_host'),
-	    database: global.config.get('mysql_db'),
-	    insecureAuth: true
-	});
+
+	var connectionObject = {
+		user: global.config.get('mysql_user'),
+		password: global.config.get('mysql_pass'),
+		host: global.config.get('mysql_host'),
+		database: global.config.get('mysql_db'),
+		insecureAuth: true
+	};
+
+	this.mysql = _mysql.createConnection(connectionObject);
 
 	this.mysql.connect(function(err) {
 		if (err)
 			console.log(err);
+	});
+
+	this.mysql.on('error', function(err) {
+		if (!err.fatal) {
+			return;
+		}
+
+		if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+			throw err;
+		}
+
+		this.mysql = _mysql.createConnection(connectionObject);
+
+		this.mysql.connect(function(err) {
+			if (err)
+				console.log(err);
+		});
 	});
 }
 
@@ -26,9 +46,9 @@ DB.prototype = {
 		chars = message.length;
 		words = message.split(' ').length;
 		
-		user = this.mysql_real_escape_string(from);
-		chan = this.mysql_real_escape_string(chan);
-		message = this.mysql_real_escape_string(message);
+		user = this.mysql.escape(from);
+		chan = this.mysql.escape(chan);
+		message = this.mysql.escape(message);
 		
 		// Log message in raw_logs
 		sql = this.squel.insert()
@@ -46,36 +66,13 @@ DB.prototype = {
 		});
 		
 		// Update user stats
+		// Should be replaced by squel, but I am not sure how it handles complex statements
 		sql = "INSERT INTO user_stats (`user`, `lines`, `words`, `chars`, `time`) VALUES ('"+from+"', '1', '"+words+"', '"+chars+"', '"+time+"') \
 			ON DUPLICATE KEY UPDATE `lines`=`lines`+1, words=words+"+words+", chars=chars+"+chars+", `time`='"+time+"'";
 
 		this.mysql.query(sql, function (error) {
 			if (error) {
 				console.log("MySQL Error [52]: " + error);
-			}
-		});
-	},
-	mysql_real_escape_string: function(str) {
-		return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
-			switch (char) {
-				case "\0":
-				return "\\0";
-				case "\x08":
-				return "\\b";
-				case "\x09":
-				return "\\t";
-				case "\x1a":
-				return "\\z";
-				case "\n":
-				return "\\n";
-				case "\r":
-				return "\\r";
-				case "\"":
-				case "'":
-				case "\\":
-				case "%":
-				return "\\"+char; // prepends a backslash to backslash, percent,
-				// and double/single quotes
 			}
 		});
 	}
