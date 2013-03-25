23f92a5bf464d7bfca81ad59f13b51db2a4a24ec
verath <verath@ibepeter>
2013-03-25 10:52:03 +0100

    Changed the result listener to collect all chunks of data. Should fix #23

--- modules/issues.js
+++ modules/issues.js
@@ -1,30 +1,39 @@
 var Issues = function() {
 	//Constructor
-}
+};
 
 Issues.prototype = {
 	https: require('https'),
+
 	handle: function(from, chan, message) {
 		var irc = global.irc;
 
-		if (matches = message.match(/^!([\S]*)$/i)) {
-			if (matches[1] == "issues") {
-				var req = this.https.request('https://api.github.com/repos/HackThis/NexBot/issues', function(res) {
-					res.on('data', function(d) {
-						// Get the result and turn it into a JSON object.
-						var obj = JSON.parse(d);
-						// Count number of issues
-						var count = obj.length;
-						// Return the string.
-						irc.client.say(chan, "NexBot has " + count + " open issue" + (count==1?'':'s') + " [http://git.io/hjkhrA]");
-					});
-				});
-				req.end();
+		if (message.length === '!issues'.length && message.indexOf('!issues') === 0) {
+			var req = this.https.request('https://api.github.com/repos/HackThis/NexBot/issues', function(res) {
+				var data = '';
 
-				req.on('error', function(e) {
-					irc.client.say(chan, "Errm there's be an error");
+				res.on('data', function(chunk) {
+					data += chunk;
 				});
-			}
+
+				res.on('end', function() {
+					// Get the result and turn it into a JSON object.
+					try {
+						var obj = JSON.parse(data);
+						var count = obj.length;
+						irc.client.say(chan, "NexBot has " + count + " open issue" + (count > 1 ? 's' : '') + " [http://git.io/hjkhrA]");
+					} catch (e) {
+						console.log(e);
+					}
+				});
+			});
+
+			req.end();
+
+			req.on('error', function(error) {
+				irc.client.say(chan, "Errm there's be an error");
+				console.log(error);
+			});
 		}
 	}
 };
