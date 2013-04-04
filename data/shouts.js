1224d20cb412382d989ebca80b5ba955b9c15a83
flabbyrabbit <flabbyrabbit@gmail.com>
2013-04-04 17:28:59 +0100

    Added whatbutton and noidea2 shouts

--- data/shouts.js
+++ data/shouts.js
@@ -1,8 +1,10 @@
 {
-	"fuckthis":"http://static.fjcdn.com/gifs/FUCK_449486_3018675.gif",
-	"headdesk":"http://nimga.com/f/Wh9jL.gif",
-	"mindblown":"https://i.chzbgr.com/completestore/12/5/18/Ydzjat1WwkugANJC7nT_sg2.gif",
-	"noidea":"http://i.imgur.com/McMJXzD.jpg",
-	"hellodog":"http://pigroll.com/img/hello_yes_this_is_dog.jpg",
-	"notime":"https://lh6.googleusercontent.com/-9-O9Kgnghc0/TlEeL3AZ15I/AAAAAAAABCg/HMOyRCLTL0Q/s800/no-time-to-explain.jpg"
+	"fuckthis":"http://goo.gl/m2RCE",
+	"headdesk":"http://goo.gl/EJy54",
+	"mindblown":"http://goo.gl/vuEy9",
+	"noidea":"http://goo.gl/yCFFA",
+	"noidea2":"http://goo.gl/KCpBr",
+	"hellodog":"http://goo.gl/JFnxD",
+	"whatbutton":"http://goo.gl/Q8B3y",
+	"notime":"http://goo.gl/exuKV"
 }
