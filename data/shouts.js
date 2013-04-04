2c9ad869be79c893a5f905148c04a58d80320399
flabbyrabbit <flabbyrabbit@gmail.com>
2013-04-04 17:17:28 +0100

    noidea shout added

--- data/shouts.js
+++ data/shouts.js
@@ -1,5 +1,6 @@
 {
 	"fuckthis":"http://static.fjcdn.com/gifs/FUCK_449486_3018675.gif",
 	"headdesk":"http://nimga.com/f/Wh9jL.gif",
-	"mindblown":"https://i.chzbgr.com/completestore/12/5/18/Ydzjat1WwkugANJC7nT_sg2.gif"
+	"mindblown":"https://i.chzbgr.com/completestore/12/5/18/Ydzjat1WwkugANJC7nT_sg2.gif",
+	"noidea":"http://i.imgur.com/McMJXzD.jpg"
 }
