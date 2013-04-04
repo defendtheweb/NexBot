c611767b62cf018beb25eb5d1998df1bbc83336e
Pete Maynard <pgm987@gmail.com>
2013-04-04 18:25:17 +0200

    Added hellodog and notime

--- data/shouts.js
+++ data/shouts.js
@@ -2,5 +2,7 @@
 	"fuckthis":"http://static.fjcdn.com/gifs/FUCK_449486_3018675.gif",
 	"headdesk":"http://nimga.com/f/Wh9jL.gif",
 	"mindblown":"https://i.chzbgr.com/completestore/12/5/18/Ydzjat1WwkugANJC7nT_sg2.gif",
-	"noidea":"http://i.imgur.com/McMJXzD.jpg"
+	"noidea":"http://i.imgur.com/McMJXzD.jpg",
+	"hellodog":"http://pigroll.com/img/hello_yes_this_is_dog.jpg",
+	"notime":"https://lh6.googleusercontent.com/-9-O9Kgnghc0/TlEeL3AZ15I/AAAAAAAABCg/HMOyRCLTL0Q/s800/no-time-to-explain.jpg"
 }
