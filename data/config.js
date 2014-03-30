ad00a2afa2fd21480827162ef998500e4b18b20a
Pete <pgm987@gmail.com>
2014-03-30 20:07:06 +0100

    Added config for heroku

---
+++ data/config.js
@@ -0,0 +1,26 @@
+{
+	"server":"irc.hackthis.co.uk",
+	"channels":["#test"],
+	"nick":"HerokuBot",
+
+	"debug": "true",
+
+	"groove":"api_key",
+
+	"lastfm_key":"api_key",
+	"lastfm_secret":"api_secrect",
+
+	"bitly_user":"username",
+	"bitly_key":"api_key",
+
+	"mysql_db":"localhost",
+	"mysql_user":"root",
+	"mysql_pass":"",
+	"mysql_db": "NexBot",
+
+	"greeting": ["Konnichiwa", "Welcome"],
+	"greeting-ignore": ["GitBot"],
+
+	"modules": ["issues"],
+	"admin": ["Osaka"]
+}
