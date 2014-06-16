6e7f62ea1d14549f3055b093209d50f0d10589af
Luke Ward <0x6C77@gmail.com>
2014-06-16 15:57:18 +0100

    Added IMDB module

---
+++ modules/imdb/imdb.js
@@ -0,0 +1,39 @@
+var IMDB = function() {
+	//Constructor
+};
+
+IMDB.prototype = {
+	http: require('http'),
+	handle: function(from, chan, message) {
+		var irc = global.irc;
+
+		var matches;
+		if (matches = message.match(/^!imdb (.+)$/i)) {
+			var term = matches[1].trim();
+
+			var req = this.http.request('http://www.omdbapi.com/?t='+term, function(res) {
+				res.setEncoding('utf8');
+				var body = '';
+				res.on('data', function(chunk) {
+					body += chunk;
+				});
+
+				res.on('end', function(){
+					if (body != 0) {
+						var obj = JSON.parse(body);
+						if (obj && obj.Title) {
+							irc.client.say(chan, obj.Title + ' | Rating: ' + obj.imdbRating + ' | ' + obj.Plot);
+						}
+					}
+				});
+			});
+			req.end();
+
+			req.on('error', function(e) {
+				irc.client.say(chan, "Errm there's be an error");
+			});
+		}
+	}
+};
+
+module.exports = new IMDB();
