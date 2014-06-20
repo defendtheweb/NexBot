5787da6d97b459f9e17ef15667454c2fdbfdb841
Luke Ward <0x6C77@gmail.com>
2014-06-20 10:45:24 +0100

    Fixed invalid hash errors in hash module

--- modules/hash/hash.js
+++ modules/hash/hash.js
@@ -13,7 +13,7 @@ Hash.prototype = {
 		if (matches = message.trimRight().match(/^!([\S]*) ([\S]+)(?: (.+))?$/i)) {
 			if (matches[1] === 'hash') {
 				var self = this;
-				var type ='';
+				var type = '';
 				var hash = matches[2].toLowerCase();
 
 				if (matches[3]) {
@@ -47,7 +47,9 @@ Hash.prototype = {
 					if (result.type !== 'plaintext') {
 						global.irc.client.say(chan, result.hashes[0].plaintext + ' | ' + result.type);
 					} else {
-						if (result.hashes[0][type]) {
+						if (type === '') {
+							global.irc.client.say(chan, 'Invalid hash');
+						} else if (result.hashes[0][type]) {
 							global.irc.client.say(chan, result.hashes[0].plaintext + ' | ' + result.hashes[0][type]);
 						} else {
 							global.irc.client.say(chan, type + ' is not supported');
