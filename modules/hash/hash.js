761b8da088cba87a2dff03eeec7341612e4680a4
Peter Eliasson <warackta@gmail.com>
2014-06-21 13:08:01 +0200

    Added a space infront of all output from hash module.
    * This was done to prevent users from making the bot execute slash commands.

--- modules/hash/hash.js
+++ modules/hash/hash.js
@@ -39,14 +39,14 @@ Hash.prototype = {
 
 				if (result.found === "true") {
 					if (result.type !== 'plaintext') {
-						global.irc.client.say(chan, result.hashes[0].plaintext + ' | ' + result.type);
+						global.irc.client.say(chan, ' ' + result.hashes[0].plaintext + ' | ' + result.type);
 					} else {
 						if (type === '') {
 							global.irc.client.say(chan, 'Invalid hash');
 						} else if (result.hashes[0][type]) {
-							global.irc.client.say(chan, result.hashes[0].plaintext + ' | ' + result.hashes[0][type]);
+							global.irc.client.say(chan, ' ' + result.hashes[0].plaintext + ' | ' + result.hashes[0][type]);
 						} else {
-							global.irc.client.say(chan, type + ' is not supported');
+							global.irc.client.say(chan, ' ' + type + ' is not supported');
 						}
 					}
 				} else {
