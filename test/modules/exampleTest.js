b6ae32ff257ef3f8766dcbf6ca4f11d0662ef363
Peter Eliasson <warackta@gmail.com>
2014-06-24 12:53:19 +0200

    Added more comments to ExampleTest.js.

--- test/modules/exampleTest.js
+++ test/modules/exampleTest.js
@@ -11,10 +11,16 @@ var sinon = require('sinon'); // SinonJS creates stubs/mocks/spies on js objects
 // Import the module to test
 var example = require("../../modules/example/example");
 
+// Each module should be encapsulated in a describe call, making
+// each module a seperate "test-suite", or group of tests that
+// belongs togheter.  
 describe('Module Example', function () {
 
+    // Before is run once before any tests in this suite.
     before(function(){
-        // We need to provide an irc client, an empty one will do
+        // We need to provide an irc client, an empty one will do.
+        // This function is later stubbed by sinon, see the 
+        // beforeEach method.
         global.irc = {
             'client': {
                 'say': function(){}
@@ -22,29 +28,65 @@ describe('Module Example', function () {
         };
     });
 
+    // After is run once after all tests in this suite
     after(function(){
+        // Clean-up after our suite. Since we defined a
+        // global object in before, delete it here
         delete global.irc;
     });
 
+    // BeforeEach is run before each test in the suite
     var stubIrcSay;
     beforeEach(function(){
         // Stub the client.say method for each test
         stubIrcSay = sinon.stub(global.irc.client, 'say');
     });
 
+    // AfterEach is run after each test in the suite
     afterEach(function(){
+        // Restore the stub so that calls in one test does
+        // not live on untill the next test
         stubIrcSay.restore();
+        // Removes any pending nock mocks (expected url calls)
         nock.cleanAll();
     });
 
+    // Each method tested is also made into a suite/group
+    // as to make the formatting of the output clearer.
     describe('#handle()', function () {
+
+        // it defines a test case. The string should describe what
+        // the test is testing, and should most likely start with
+        // "should". The function is the test to be executed.
+        // Keep each test as specific as possible.
         it('should send "Hello channel" to the channel', function () {
 
             // Fake a message to the channel testChan
             example.handle('testFrom', 'testChan', 'testMsg');
 
+            // The conditions are checked using Chai asserts.
+            // The test will complete only if all the asserts
+            // pass. The conditions here are both using the sinon
+            // stub/spy api.
             assert.isTrue(stubIrcSay.calledOnce);
             assert.deepEqual(stubIrcSay.args[0], ['testChan', 'Hello channel']);
+        });
+
+        // It is also possible to test async operations, this is
+        // done by providing a function(done){} to it. Note that if
+        // you do this, you also have to call done() when you have
+        // completed the test.
+        it('Should have sent "Hello channel" to the channel after some time', function(done) {
+            // Fake a message to the channel testChan
+            example.handle('testFrom', 'testChan', 'testMsg');
+
+            // Wait some time...
+            setTimeout(function(){
+                assert.isTrue(stubIrcSay.calledOnce);
+                assert.deepEqual(stubIrcSay.args[0], ['testChan', 'Hello channel']);
+                // Note the call to done when we have are done
+                done();
+            }, 1);
         });
     });
 
