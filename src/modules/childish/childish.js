bd1f40f179af5a2355308866c650808bb9e89f1e
flabbyrabbit <flabbyrabbit@gmail.com>
2014-09-24 15:36:20 +0100

    Added childish feature

--- src/modules/childish/childish.js
+++ src/modules/childish/childish.js
@@ -9,7 +9,15 @@ Childish.prototype = {
 		if (matches) {
 			global.irc.client.say(chan, "hehe, " + matches[1]);
 		}
+
+		var matches = message.match(/serious/i);
+		if (matches) {
+			global.irc.client.send('NICK', 'Sirius');
+			global.irc.client.say(chan, "You're not serious, I'm Sirius!");
+			global.irc.client.send('NICK', 'NexBot');
+		}
 	}
+
 };
 
 module.exports = new Childish();
